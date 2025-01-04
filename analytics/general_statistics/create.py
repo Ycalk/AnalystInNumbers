b9dc301b0f805f9663581f2cd5fc91a9c45500ed
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-01-04 22:25:49 +0500

    Update general stats: add salary by city

--- analytics/general_statistics/create.py
+++ analytics/general_statistics/create.py
@@ -36,6 +36,18 @@ class CreateGeneralStatistics:
         count_by_year.columns = ['year', 'count']
         count_by_year.to_csv(out_path, index=False)
     
+    def salary_by_city(self, out_path):
+        data = self.data.copy().dropna(subset=['salary', 'area_name'])
+        
+        data['salary'] = pd.to_numeric(data['salary'], errors='coerce')
+        data = data[(data['salary'] >= CreateGeneralStatistics.min_salary) & 
+                    (data['salary'] <= CreateGeneralStatistics.max_salary)]
+        
+        salary_by_city = data.groupby('area_name')['salary'].mean().reset_index()
+        salary_by_city.columns = ['area_name', 'average_salary']
+        salary_by_city.to_csv(out_path, index=False)
+    
     def run(self):
         self.salary_by_year(f'{self.out_folder}/salary_by_year.csv')
         self.count_by_year(f'{self.out_folder}/count_by_year.csv')
+        self.salary_by_city(f'{self.out_folder}/salary_by_city.csv')
