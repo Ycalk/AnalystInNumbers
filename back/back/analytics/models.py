498267e1977602413e1cc9bc2df4ecc51a0b1aa6
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-01-15 00:53:09 +0500

    Add models

--- back/back/analytics/models.py
+++ back/back/analytics/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class StatByYear(models.Model):
+    year = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+class StatByCity(models.Model):
+    city = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+class CountByYear(StatByYear):
+    count = models.IntegerField()
+
+    class Meta:
+        db_table = 'count_by_year'
+
+class SalaryByYear(StatByYear):
+    salary = models.FloatField()
+
+    class Meta:
+        db_table = 'salary_by_year'
+
+class SalaryByCity(StatByCity):
+    salary = models.FloatField()
+
+    class Meta:
+        db_table = 'salary_by_city'
+
+
+
