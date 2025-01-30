6a8f22877b1fa7e45551fe67a7fe5b3188881a3a
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-01-30 13:33:54 +0500

    Update footer text

--- front/lib/presentation/widgets/footer.dart
+++ front/lib/presentation/widgets/footer.dart
@@ -12,12 +12,7 @@ class Footer extends StatelessWidget {
       color: AppColors.primaryLight,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SelectableText('Analyst In Numbers', style: TextStyles.subtitle.copyWith(color: Colors.white)),
-            SelectableText('Лосев Алексей Ярославович, АТ-22', style: TextStyles.description.copyWith(color: Colors.white)),
-          ],
-        ),
+        child: SelectableText('Analyst In Numbers', style: TextStyles.subtitle.copyWith(color: Colors.white)),
       ),
     );
   }
